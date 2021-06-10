578f93a769bcc6f2143fe73518f0edbabaee309f
KseniyaRadzkova <kseniya.goncharuk@mail.ru>
2021-06-10 10:39:26 +0300

    homework 10

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,22 @@
 const initState = {
-
+    isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionLoadingType): typeof initState=> { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case "CHANGE_LOADING": {
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+type ActionLoadingType = {
+    type: "CHANGE_LOADING"
+    isLoading: boolean
+}
+
+export const loadingAC = (isLoading: boolean): ActionLoadingType => ({type: "CHANGE_LOADING", isLoading})
